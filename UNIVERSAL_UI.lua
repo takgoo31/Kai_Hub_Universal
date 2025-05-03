6e13cdd57b595a920c7d3693b1ae8c8165c94eba
takgoo31 <takgoo.official@gmail.com>
2025-05-04 00:12:20 +0800

    Update UNIVERSAL_UI.lua

--- UNIVERSAL_UI.lua
+++ UNIVERSAL_UI.lua
@@ -1,6 +1,65 @@
 --THIS FILE WAS MADE BY: KAI SCRIPTS!!!
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
-local Window = Fluent.CreateLib("Kai Hub | Universal", "DarkTheme")
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Random TEST!")
+local Window = Fluent:CreateWindow({
+    Title = "Kai Hub | Universal " .. Fluent.Version,
+    SubTitle = "by Kai Team",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Blox Fruits = Window.AddTab({ Title = "Blox Fruits", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+do
+    Fluent:Notify({
+        Title = "Welcome to Kai Hub!",
+        Content = "This version is the universal version! Explore our script. Thank you! ",
+        SubContent = "READ: IF YOU CLOSE OUR UI/SCRIPT, RE-EXECUTE IT TO LOAD AGAIN!", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
+
+  Tabs.Main:AddParagraph({
+        Title = "Kai Hub",
+        Content = "Kai Hub is a type of script on Roblox that you can use FREE!"
+    })
+
+  Tabs.Main:AddButton({
+        Title = "Kai Hub Community",
+        Description = "Join to our COMMUNITY! (CLICK HERE)",
+        Callback = function()
+      })
+            Window:Dialog({
+                Title = "Kai Hub | Universal",
+                Content = "Copy our Discord Server Link",
+                Buttons = {
+                    {
+                        Title = "Copy",
+                        Callback = function()
+                            setclipboard("https://discord.com/invite/wDMPK3QAmY")
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                            print("Cancelled.")
+                        end
+                    }
+                }
+            })
+        end
+    })
+
+
+
+  
