2f04334856d6ca055aa69cf788be0351b66280eb
takgoo31 <takgoo.official@gmail.com>
2025-05-03 23:53:28 +0800

    Update UNIVERSAL_UI.lua

--- UNIVERSAL_UI.lua
+++ UNIVERSAL_UI.lua
@@ -1,6 +1,6 @@
 --THIS FILE WAS MADE BY: KAI SCRIPTS!!!
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-local Window = Library.CreateLib("Kai Hub | Universal", "DarkTheme")
+local Window = Fluent.CreateLib("Kai Hub | Universal", "DarkTheme")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Random TEST!")
