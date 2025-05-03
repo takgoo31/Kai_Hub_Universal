5b883199e4fb7118c41a31e1bb97a99c24fbabea
takgoo31 <takgoo.official@gmail.com>
2025-05-03 23:45:18 +0800

    Create UNIVERSAL_UI.lua
    new!

---
+++ UNIVERSAL_UI.lua
@@ -0,0 +1,5 @@
+--THIS FILE WAS MADE BY: KAI SCRIPTS!!!
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Kai Hub | Universal", "DarkTheme")
+local Tab = Window:NewTab("Main")
