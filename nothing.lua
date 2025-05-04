4b3d44cf9d994108357e63ecbc1920f1c128af39
takgoo31 <takgoo.official@gmail.com>
2025-05-04 12:37:05 +0800

    Update nothing.lua

--- nothing.lua
+++ nothing.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/ra...)
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Kai Hub | Universal",
